4ef8c5bedc8363be5308f3293ec3a46bc56dd7e6
Andres <andrescantlon@gmail.com>
2023-04-23 02:25:14 -0300

    Validaciones Comienzo

--- Components/FormularioRegistro/Cartelito.js
+++ Components/FormularioRegistro/Cartelito.js
@@ -1,14 +1,31 @@
 import { Text, View } from "react-native";
-import react from "react";
+import { Button } from "react-native-paper";
+import { useDispatch } from "react-redux";
+import mailVerificarUsuario from "../../Actions/mailVerificarUsuario";
+import { useNavigation } from '@react-navigation/native';
 
-export default function Cartelito() {
+export default function Cartelito({ input }) {
+
+    const dispatch = useDispatch();
+    const navigation = useNavigation();
+
+    function handleOnClick() {
+        dispatch(mailVerificarUsuario(input))
+        navigation.navigate('HomePage')
+    }
 
     return (
 
         <View>
             <Text>
-                Cartelito que avisa que te registrarte con exito y debes ir a validar tu mail
+                Te has registrado correctamente. Valida tu mail para poder comenzar a utilizar todas las funciones de Adopt.Me
             </Text>
+
+            <Button
+                style={{ backgroundColor: "grey"}}
+                title="Validar E-Mail"
+                onPress={handleOnClick}
+        />
         </View>
     )
 }
